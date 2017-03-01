d75575fc4f96501b6d56fb7f0593accc7b4f4b5e
justin <ham896@mail.usask.ca>
2017-02-28 20:36:27 -0600

    squirtle squirtle                _,........__             ,-'            "`-.           ,'                   `-.         ,'                        \       ,'                           .       .'\               ,"".       `      ._.'|             / |  `       \      |   |            `-.'  ||       `.      |   |            '-._,'||       | \      .`.,'             `..,'.'       , |`-.      l                       .'`.  _/  |   `.      `-.._'-   ,          _ _'   -" \  .     ` `."""""'-.`-...,---------','         `. `....__. .'        `"-..___      __,'\          \  \     \ \_ .          |   `""""'    `.           . \     \   `.          |              `.          |  .     L     `.        |`--...________.'.        j   |     |       `._    .'      |          `.     .|   ,     |          `--,\       .            `7""' |  ,      |             ` `      `            /     |  |      |    _,-'"""`-.              \ `.     .          /      |  '      |  ,'          `.               \  v.__  .        '       .   \    /| /              \                \/    `""\"""""""`.       \   \  /.''                |                 `        .        `._ ___,j.  `/ .-       ,---.     |                 ,`-.      \         ."     `.  |/        j     `    |                /    `.     \       /         \ /         |     /    j               |       `-.   7-.._ .          |"          '         /               |          `./_    `|          |            .     _,'               `.           / `----|          |-............`---'                 \          \      |          |                ,'           )     `.         |                 7____,,..--'      /          |                                   `---.__,--.'

--- GameBoard.py
+++ GameBoard.py
@@ -4,10 +4,6 @@ import unittest
 from Position import Pos
 import sys
 
-HEIGHT = 7
-WIDTH = 7
-
-
 class IllegalMoveError(Exception):
     pass
 
@@ -15,6 +11,8 @@ KING_SQUIRTLE_SQUAD = True
 PUFFS_MAGICAL_DRAGON_SQUAD = False
 WIDTH = 7
 HEIGHT = 7
+WIN_VALUE = 1000
+WIN_FOR = KING_SQUIRTLE_SQUAD
 
 class MyTestObject:
     def __init__(self):
@@ -25,21 +23,30 @@ class MyTestObject:
 
 # TODO redesign this class for using less space
 class dictBoard:
-    def __init__(self):
-        self.board = {}
+    def __init__(self, state, player=PUFFS_MAGICAL_DRAGON_SQUAD):
+        """
 
-    def start_state(self):
-        self.numDragons = 5
-        self.numGuards = 3
-        self.turn = True
-        self.board[Pos(6,0)] = 'D'
-        self.board[Pos(6,6)] = 'D'
-        self.board[Pos(6,3)] = 'K'
-        for i in range(2,5):
-            self.board[Pos(5,i)] = 'G'
-        self.board[Pos(0,3)] = 'D'
-        self.board[Pos(1,1)] = 'D'
-        self.board[Pos(1,5)] = 'D'
+        :param state:
+        :type state: dict
+        :param player:
+        """
+
+        self.board = {}
+        if state == None:
+            self.board[Pos(6,0)] = 'D'
+            self.board[Pos(6,6)] = 'D'
+            self.board[Pos(6,3)] = 'K'
+            for i in range(2,5):
+                self.board[Pos(5,i)] = 'G'
+            self.board[Pos(0,3)] = 'D'
+            self.board[Pos(1,1)] = 'D'
+            self.board[Pos(1,5)] = 'D'
+        else:
+            self.board = state
+
+        self.whoseTurn = player
+        self.cachedWin = False
+        self.cachsedWinner = None
 
     def __copy__(self):
         rv = dictBoard()
@@ -61,106 +68,161 @@ class dictBoard:
         rStr += "\n"
         return rStr
 
+    def move(self, piecePos, newPos):
+        temp = self.board.pop(piecePos)
+        self.board[newPos] = temp
+
     def get(self,key):
         return self.board.get(key)
 
     def set(self,key,value):
         self.board[key] = value
 
+    def isTerminal (self):
+        return self.winFor(KING_SQUIRTLE_SQUAD) or self.winFor(PUFFS_MAGICAL_DRAGON_SQUAD) or (len(self.successors()) == 0)
 
-def legal_moves(board,piecePos):
-    """
-    Takes a piece position on the board, returns all legal moves
-    :param board:
-    :type board: {"Pos":Char}
-    :param piecePos:
-    :type piecePos: Pos
-    :return: List of legal moves for a piece
-    :rtype: [Pos]
-    """
-    rList = []
-    #legal move for all pieces
-    rList.append((piecePos+Pos(1,0)))
-    rList.append((piecePos+Pos(-1,0)))
-    rList.append((piecePos+Pos(0,1)))
-    rList.append((piecePos+Pos(0,-1)))
-    # if King
-    # TODO better way to do this
-    if board.get(piecePos) == 'K':
-        kList = []
-        temp = 0
-        for x in rList:
-            if board.get(x) == 'G':
-                if temp == 0:
-                    kList.append((x+Pos(1,0)))
-                elif temp == 1:
-                    kList.append(x+Pos(-1,0))
-                elif temp == 2:
-                    kList.append(x+Pos(0,1))
-                else:
-                    kList.append(x+Pos(0,-1))
-            temp += 1
-        rList.extend(kList)
-    elif board.get(piecePos) == 'D':
-        rList.append((piecePos+Pos(1,1)))
-        rList.append((piecePos+Pos(1,-1)))
-        rList.append((piecePos+Pos(-1,1)))
-        rList.append((piecePos+Pos(-1,-1)))
-    return rList
+    def legal_moves(self, piecePos):
+        """
+        Takes a piece position on the board, returns all legal moves
+        :param board:
+        :type board: {"Pos":Char}
+        :param piecePos:
+        :type piecePos: Pos
+        :return: List of legal moves for a piece
+        :rtype: [Pos]
+        """
+        rList = []
+        # legal move for all pieces
+        rList.append((piecePos + Pos(1, 0)))
+        rList.append((piecePos + Pos(-1, 0)))
+        rList.append((piecePos + Pos(0, 1)))
+        rList.append((piecePos + Pos(0, -1)))
+        # if King
+        # TODO better way to do this
+        if self.board.get(piecePos) == 'K':
+            kList = []
+            temp = 0
+            for x in rList:
+                if self.board.get(x) == 'G':
+                    if temp == 0:
+                        kList.append((x + Pos(1, 0)))
+                    elif temp == 1:
+                        kList.append(x + Pos(-1, 0))
+                    elif temp == 2:
+                        kList.append(x + Pos(0, 1))
+                    else:
+                        kList.append(x + Pos(0, -1))
+                temp += 1
+            rList.extend(kList)
+        elif self.board.get(piecePos) == 'D':
+            rList.append((piecePos + Pos(1, 1)))
+            rList.append((piecePos + Pos(1, -1)))
+            rList.append((piecePos + Pos(-1, 1)))
+            rList.append((piecePos + Pos(-1, -1)))
+        return rList
 
-# TODO shrink get_legal_moves to legal_moves and call this in legal_moves
+    def get_legal_moves(self, piecePos):
+        move_list = legal_moves(self.board, piecePos)
+        rList = list()
+        while len(move_list) > 0:
+            x = move_list.pop()
+            if (x.x >= 0) & (x.y >= 0) & (x.x < HEIGHT) & (x.y < WIDTH) & (self.board.get(x) is None):
+                rList.append(x)
+        return rList
 
-
-def get_legal_moves(board,piecePos):
-    move_list = legal_moves(board,piecePos)
-    rList = list()
-    while len(move_list)>0:
-        x = move_list.pop()
-        if(x.x>=0) & (x.y>=0) & (x.x<HEIGHT) & (x.y<WIDTH) & (board.get(x) is None):
-            rList.append(x)
-    return rList
-
-
-def move(board, piecePos, newPos):
-    temp = board.pop(piecePos)
-    board[newPos] = temp
+    # TODO shrink get_legal_moves to legal_moves and call this in legal_moves
 
 
-# Modify this for use with the get_legal_moves function
-def this_one(bClass, piecePos, newPos):
-    if bClass.board.get(piecePos) == None:
-        raise IllegalMoveError("Invalid Piece")
+    # Modify this for use with the get_legal_moves function
 
-    l = get_legal_moves(bClass.board,piecePos)
+    # TODO check if a guard is captured
 
-    # **********************************************Use index
-    b = dictBoard()
-    b.board = bClass.board.copy()
-    if (l.count(newPos) <= 0):
-        raise IllegalMoveError("NOOOOOOO! An illegal move!")
+    def this_one(self, piecePos, newPos):
 
-    # TODO Count is terrible use index
-    move(b.board,piecePos,newPos)
-    return b
+        # TODO check whose turn and piece
+        # TODO if king or gurads can capture a drangon
+
+        if self.board.get(piecePos) == None:
+            raise IllegalMoveError("Invalid Piece")
+
+        l = get_legal_moves(self.board, piecePos)
+
+        # **********************************************Use index
+        b = dictBoard()
+        b.board = self.board.copy()
+        if (l.count(newPos) <= 0):
+            raise IllegalMoveError("NOOOOOOO! An illegal move!")
+
+        # TODO Count is terrible use index
+        move(b.board,piecePos,newPos)
+        return (b, piecePos, newPos)
+
+    def successors(self):
+        rList = []
+        for p in self.teamPieces():
+            for m in self.get_legal_moves(p):
+                rList.append(self.this_one(p,m))
+        return rList
+
+    def winFor(self, player):
+        if(self.cachedWin == False):
+            won = False
+            King_pos = None
+            for key, value in pieces:
+                if value == 'K':
+                    King_pos = key
+            assert King_pos != None
+
+            if(player==KING_SQUIRTLE_SQUAD):
+                pieces = self.board.items()
+                if King_pos[0] == 0:
+                    won = True
+            elif(player==PUFFS_MAGICAL_DRAGON_SQUAD):
+                raise Exception()
+                # TODO check if king is captured
+                # TODO cehck if tie
+                    # case 1: two or less drangon king wins
+                    # case 2: three or more drangons and no gaurds, dragons win
+            if(won):
+                self.cachedWin = True
+                self.cachsedWinner = player
+                return True
+            else:
+                return False
+        else:
+            return player == self.cachsedWinner
 
 
-# TODO: add function to make board list, for all pieces whose turn it is
-
-def teamMoves(bClass):
-    potentialMoves = []
-    temp = bClass.board.items()
-    if bClass.turn == KING_SQUIRTLE_SQUAD:
-        for i in temp:
-            if i[1] == 'G' or i[1] == 'K':
-                potentialMoves.append(i)
-    else:
-        for i in temp:
-            if i[1] == 'D':
-                potentialMoves.append(i)
-    print(potentialMoves)
+    def utility(self):
+        if(self.winFor(WIN_FOR)):
+            return WIN_VALUE
+        elif(self.winFor(not WIN_FOR)):
+            return -WIN_VALUE
+        else:
+            return 0
 
 
 
+
+
+
+
+
+#TODO: add function to make board list, for all pieces whose turn it is
+
+    def teamPieces(self):
+        potentialMoves = []
+        temp = self.board.items()
+        if self.whoseTurn == KING_SQUIRTLE_SQUAD:
+            for i in temp:
+                if i[1] == 'G' or i[1] == 'K':
+                    potentialMoves.append(i)
+        else:
+            for i in temp:
+                if i[1] == 'D':
+                    potentialMoves.append(i)
+
+        return potentialMoves
 
 
 class BoardTests(unittest.TestCase):
